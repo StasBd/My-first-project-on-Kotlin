95525e5efe35b6a850bf8d62d3a4cc47ae2e3204
Stanislav <129835954+StasBd@users.noreply.github.com>
2023-08-09 18:58:17 +0300

    Add files via upload

---
+++ src/main/kotlin/helloname.kt
@@ -0,0 +1,5 @@
+fun main() {
+    var name: String = "Stas" //There can be any another name!
+    var helloname = "Hello $name, Kotlin journey has started!" //Using Kotling formating
+    println(helloname)
+}
